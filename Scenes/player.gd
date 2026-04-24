91bd521c5a27396e39c99122f40d9b911265b087
Miguel Kemel Zago <533637@edu.ishilversum.nl>
2026-04-24 15:39:08 +0200

    jump animations

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -7,25 +7,36 @@ const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# 1. Gravity toevoegen
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# 2. Springen
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# 3. Richting en snelheid
 	var direction := Input.get_axis("left", "right")
-
 	if direction:
 		velocity.x = direction * SPEED
-		animator.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animator.play("idle")
 
+	# 4. ANIMATIE LOGICA (Belangrijk!)
+	if not is_on_floor():
+		# Prioriteit: We zijn in de lucht
+		if velocity.y < 0:
+			animator.play("jump")
+		else:
+			animator.play("fall")
+	else:
+		# We zijn op de grond
+		if direction != 0:
+			animator.play("run")
+		else:
+			animator.play("idle")
+
+	# 5. Omdraaien (Flippen)
 	if direction > 0:
 		animator.flip_h = false
 	elif direction < 0:
