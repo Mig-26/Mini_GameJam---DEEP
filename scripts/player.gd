810ffd4eab54ba3fd6e1664035c9b887f9bce8b1
Mig-26 <533637@edu.ishilversum.nl>
2026-04-25 00:10:59 +0200

    coyote time and falling through platforms

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,15 +5,33 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+var coyote_time := 0.1
+var coyote_timer := 0.0
+
+func _ready():
+	set_collision_mask_value(2, true)
+
+func _fallThroughPlatforms():
+	if Input.is_action_just_pressed("down"):
+		set_collision_mask_value(2, false)
+	elif Input.is_action_just_released("down"):
+		set_collision_mask_value(2, true)
 
 func _physics_process(delta: float) -> void:
 	# gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# coyote time update
+	if is_on_floor():
+		coyote_timer = coyote_time
+	else:
+		coyote_timer -= delta
+
 	# jumping
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and coyote_timer > 0:
 		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0
 
 	# moving
 	var direction := Input.get_axis("left", "right")
@@ -41,10 +59,5 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animator.flip_h = true
 
-	#falling through platforms
-	set_collision_mask_value(2, true)
-
-	if Input.is_action_just_pressed("down"):
-			position.y += 1 
-
+	_fallThroughPlatforms()
 	move_and_slide()
